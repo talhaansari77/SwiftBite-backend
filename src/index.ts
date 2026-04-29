0ed4b704d3ed77f4222e18d3c040c9a0ac8218c4
Muhammad Talha Tahir <m.tahir@paaet.edu.kw>
2026-04-29 12:46:01 +0300

    Fix CORS for production

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}))
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
